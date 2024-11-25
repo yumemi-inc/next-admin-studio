fabb6d01ef0d287246b990e49394e1034904ecdb
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-25 15:43:50 +0900

    build: add Administrator avatar container to header

--- src/app/(general)/_layout/header.tsx
+++ src/app/(general)/_layout/header.tsx
@@ -1,9 +1,12 @@
 import { Flex, Text } from "@mantine/core";
 
+import { AdministratorAvaterContainer } from "@/model/administrator/components/avater/container";
+
 export const Header = () => {
   return (
     <Flex justify="space-between" align="center" py="xs" px="md">
       <Text fw="bold">Next Admin Studio</Text>
+      <AdministratorAvaterContainer />
     </Flex>
   );
 };
