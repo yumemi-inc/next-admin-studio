b00ce29f78a1af6f75b114087be01531097698a8
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-25 15:27:56 +0900

    build: add NotFound component to handle 404 errors

---
+++ src/app/(general)/not-found.tsx
@@ -0,0 +1,11 @@
+import { Stack, Title } from "@mantine/core";
+
+export default function NotFound() {
+  return (
+    <main>
+      <Stack p="xl">
+        <Title>Not found!😢</Title>
+      </Stack>
+    </main>
+  );
+}
