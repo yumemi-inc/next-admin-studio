7e2b226326fed48e8ca3f778e39fc41d8df1b7f2
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-26 13:30:26 +0900

    refactor: add missing import for NullableDateRange in form type definition

--- src/model/artwork/components/form-with-preview/form/type.ts
+++ src/model/artwork/components/form-with-preview/form/type.ts
@@ -1,4 +1,5 @@
 import type { NullableDateRange } from "@/common/components/form/date-time-range-input/type";
+
 import type { ArtworkSalesStyleInputSlice } from "./inputs/sales-style/slice";
 
 export type ArtworkForm = {
