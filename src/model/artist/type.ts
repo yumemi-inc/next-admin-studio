04f7675c0569862ed1766d43b498e00c7521ccfc
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-26 14:18:35 +0900

    feat: add new fields to Artist type for name, tags, and authorization status

--- src/model/artist/type.ts
+++ src/model/artist/type.ts
@@ -3,5 +3,8 @@ import type { ContentStatus } from "@/model/common/const/content-status";
 export type Artist = {
   id: string;
   adminLabel: string;
+  name: string;
+  tags: string[];
+  authorized: boolean;
   creationStatus: ContentStatus;
 };
