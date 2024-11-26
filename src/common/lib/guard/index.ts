8d7e9fa17e021e59ee0bf18038057e92db57b7b8
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-26 10:08:58 +0900

    feat: add isNonNull utility function to guard exports

--- src/common/lib/guard/index.ts
+++ src/common/lib/guard/index.ts
@@ -1,4 +1,4 @@
-export { isEmpty, isNullish, isDefined } from "remeda";
+export { isEmpty, isNullish, isDefined, isNonNull } from "remeda";
 export { isBelowMaxLength } from "./is-below-max-length";
 export { isUrl } from "./is-url";
 export { isBefore } from "./is-before";
