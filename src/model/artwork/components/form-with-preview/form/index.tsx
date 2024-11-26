3c8309c7594f7d7f7526ee34c0308c4163ec9777
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-26 13:30:20 +0900

    refactor: reorganize imports in ArtworkForm component for clarity

--- src/model/artwork/components/form-with-preview/form/index.tsx
+++ src/model/artwork/components/form-with-preview/form/index.tsx
@@ -1,22 +1,14 @@
-import { ArtworkConditionInput } from "./inputs/condition";
-
-import { ArtworkSalesStyleInput } from "./inputs/sales-style";
-
-import { ArtworkSalesPeriodInput } from "./inputs/sales-period";
-
-import { ArtworkOpenAtInput } from "./inputs/open-at";
-
-import { ArtworkImageInput } from "./inputs/image";
-
-import { ArtworkArtistInput } from "./inputs/artist";
-
-import { ArtworkDescriptionInput } from "./inputs/description";
-
-import { ArtworkTitleInput } from "./inputs/title";
-
 import type { FC } from "react";
 
 import { ArtworkAdminLabelInput } from "./inputs/admin-label";
+import { ArtworkArtistInput } from "./inputs/artist";
+import { ArtworkConditionInput } from "./inputs/condition";
+import { ArtworkDescriptionInput } from "./inputs/description";
+import { ArtworkImageInput } from "./inputs/image";
+import { ArtworkOpenAtInput } from "./inputs/open-at";
+import { ArtworkSalesPeriodInput } from "./inputs/sales-period";
+import { ArtworkSalesStyleInput } from "./inputs/sales-style";
+import { ArtworkTitleInput } from "./inputs/title";
 import { ArtworkFormWrapper } from "./wrapper";
 
 export const ArtworkForm: FC = () => {
