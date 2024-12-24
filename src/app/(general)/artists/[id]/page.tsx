7ed566c9eeb46b5272a1f575e04247ad3483bffb
yukendev <yuken0906dev@gmail.com>
2024-12-24 11:09:50 +0900

    feat: generateStaticParams

--- src/app/(general)/artists/[id]/page.tsx
+++ src/app/(general)/artists/[id]/page.tsx
@@ -1,4 +1,11 @@
 import { ArtistFormWithPreview } from "@/model/artist/components/form-with-preview";
+import { ARTIST_MOCK_DATA } from "@/model/artist/mock";
+
+export function generateStaticParams() {
+  return ARTIST_MOCK_DATA.map((artist) => ({
+    id: artist.id,
+  }));
+}
 
 export default function ArtistDetailPage() {
   return (
