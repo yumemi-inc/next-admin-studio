9c8c86344ed1c376ab47a4c9a26f732f88496679
yukendev <yuken0906dev@gmail.com>
2024-12-24 10:27:14 +0900

    feat: config for static output

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   experimental: {
     // see: https://mantine.dev/guides/next/#app-router-tree-shaking
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
